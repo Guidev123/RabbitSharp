810376112b075dd7bf692fe58a2988e84a093602
Guilherme Rafael <155389912+Guidev123@users.noreply.github.com>
2025-08-10 02:08:16 -0300

    Documentation improvement

--- src/RabbitSharp/Builders/PublisherOptionsBuilder.cs
+++ src/RabbitSharp/Builders/PublisherOptionsBuilder.cs
@@ -2,10 +2,22 @@
 
 namespace RabbitSharp.Builders
 {
+    /// <summary>
+    /// Provides a fluent API for configuring and building <see cref="PublisherOptions"/> instances.
+    /// </summary>
     public sealed class PublisherOptionsBuilder
     {
         private readonly PublisherOptions _config = new();
 
+        /// <summary>
+        /// Configures the exchange settings for the publisher.
+        /// </summary>
+        /// <param name="configure">
+        /// An action that receives an <see cref="ExchangeBuilder"/> to define the exchange configuration.
+        /// </param>
+        /// <returns>
+        /// The current <see cref="PublisherOptionsBuilder"/> instance for method chaining.
+        /// </returns>
         public PublisherOptionsBuilder WithExchange(Action<ExchangeBuilder> configure)
         {
             var builder = new ExchangeBuilder(_config.Exchange);
@@ -13,12 +25,33 @@ namespace RabbitSharp.Builders
             return this;
         }
 
+        /// <summary>
+        /// Sets the routing key to be used when publishing messages.
+        /// </summary>
+        /// <param name="routingKey">
+        /// The routing key that determines how messages are routed by the exchange.
+        /// </param>
+        /// <returns>
+        /// The current <see cref="PublisherOptionsBuilder"/> instance for method chaining.
+        /// </returns>
         public PublisherOptionsBuilder WithRoutingKey(string routingKey)
         {
             _config.RoutingKey = routingKey;
             return this;
         }
 
+        /// <summary>
+        /// Builds and returns the configured <see cref="PublisherOptions"/> instance.
+        /// </summary>
+        /// <remarks>
+        /// This method will throw an <see cref="InvalidOperationException"/> if a routing key is not specified.
+        /// </remarks>
+        /// <returns>
+        /// A fully configured <see cref="PublisherOptions"/> instance.
+        /// </returns>
+        /// <exception cref="InvalidOperationException">
+        /// Thrown if no routing key has been set before calling this method.
+        /// </exception>
         public PublisherOptions Build()
         {
             if (string.IsNullOrEmpty(_config.RoutingKey))
